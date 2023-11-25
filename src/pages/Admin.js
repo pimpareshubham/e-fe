184a53a70a35b01e9ddc0c0b286ee96b13edc493
pimpareshubham <127371609+pimpareshubham@users.noreply.github.com>
2023-11-25 11:38:57 +0530

    Update Admin.js

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Admin = () => {
   const [email, setEmail] = useState('');
