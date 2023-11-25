4e559b123adc7d9e2e8efa2b53e1bc600ff740fe
pimpareshubham <127371609+pimpareshubham@users.noreply.github.com>
2023-11-25 11:40:16 +0530

    Update App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   const [file, setFile] = useState(null);
   const [uploadedFiles, setUploadedFiles] = useState([]);
