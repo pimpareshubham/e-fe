da5906604b8e81a3d922631eb6b3eabf188443ac
pimpareshubham <127371609+pimpareshubham@users.noreply.github.com>
2023-11-25 11:40:28 +0530

    Update ContactUs.js

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import myImage1 from '../img/cu.jpg';
 
 const ContactUs = () => {
